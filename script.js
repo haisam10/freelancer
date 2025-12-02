424cc26dab692de29bf6b55030d04513d5db89eb
Haisam <122108214+haisam10@users.noreply.github.com>
2025-12-02 20:58:58 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -8,3 +8,11 @@ projectcard.from(".project-card", {
     stagger: 0.2
   })
 
+function openImage(src) {
+  document.getElementById("popupImage").src = src;
+  document.getElementById("imagePopup").style.display = "flex";
+}
+
+function closeImage() {
+  document.getElementById("imagePopup").style.display = "none";
+}
